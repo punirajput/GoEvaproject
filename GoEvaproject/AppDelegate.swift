548679ec4eff567b3e9d66650984fe0571861efe
punirajput <112754094+punirajput@users.noreply.github.com>
2022-09-04 22:40:27 +0530

    my first commit :)

--- GoEvaproject/AppDelegate.swift
+++ GoEvaproject/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         GMSPlacesClient.provideAPIKey("AIzaSyD0-Rsg9rH_HSsVm1klolpBuIopMo6mDQA")
-      
+        print ("hello")
         return true
     }
 
